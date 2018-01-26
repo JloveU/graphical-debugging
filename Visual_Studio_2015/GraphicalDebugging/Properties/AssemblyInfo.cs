9bb9fa6ab7cbbdc0ae1e770e3cab0037557d3570
yuqiang <932967248@qq.com>
2018-01-26 10:40:38 +0800

    Reconfigurated project files to VS2017.

--- Visual_Studio_2015/GraphicalDebugging/Properties/AssemblyInfo.cs
+++ Visual_Studio_2015/GraphicalDebugging/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GraphicalDebugging")]
-[assembly: AssemblyDescription("Visual Studio 2015 Graphical Debugging Tools")]
+[assembly: AssemblyDescription("Visual Studio 2017 Graphical Debugging Tools")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GraphicalDebugging")]
